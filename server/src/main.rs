fbeb08cd95bcdc1bcee7420ed5c83d758f39bdaf
tuna2134 <hello@tuna2134.dev>
2024-12-24 07:10:23 +0000

    axum...

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,12 @@
+use tokio::net::TcpListener;
+use axum::{routing::get, Router};
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, app).await?;
     Ok(())
 }
